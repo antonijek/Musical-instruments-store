e6ba5b425f89c43b47796d6541a3fffb142b6da9
eris00 <erissutkovic00@gmail.com>
2022-07-24 18:24:08 +0200

    fix

--- src/components/menu/Feed.js
+++ src/components/menu/Feed.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { React, useState } from "react";
 import InstrumentCard from "./InstrumentCard";
 import { UserContext } from "../UserContext";
 import {
